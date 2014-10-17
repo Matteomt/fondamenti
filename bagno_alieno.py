6afdc6d13bb51c60f8a7f349ee420ef7d1926613
Matteo <matteo.terruzzi@gmail.com>
2014-10-17 19:06:03 +0200

    Update bagno_alieno.py

--- bagno_alieno.py
+++ bagno_alieno.py
@@ -3,21 +3,26 @@
 "Ritorna: la dimensione delle piastrelle da usare per salvare il pianeta Terra dall'attacco alieno.
 
 def MCD(a,b):
-  if a*b==0:    "Se uno dei due è 0,
-    return a+b  "ritorna l'altro.
-  elif a>b:     "L'uso di `elif` e di una condizione in più si può risolvere con l'impiego di 2 funzioni.
-    return MCD(a%b,b)
-  else:
-    return MCD(b%a,a)
+  if a%b==0:
+    return b
+  else
+    return MCD(b,a%b)
 
-def MCD2(a,b):
-  if a>b:
-    return MCD2_ordinato(a,b)
-  else:
-    return MCD2_ordinato(b,a)
-
-def MCD2_ordinato(a,b):
-  if b==0:
-    return a
-  else:
-    return MCD2(a%b,b)
+"Perché l'MCD?
+"Viene dato che il costo totale della commissione (da minimizzare) è proporzionale al lato delle piastrelle (tutte uguali, quadrate).
+"Ciò che è rilevante qui è che le piastrelle aliene NON si pagano in base all'area, che è costante, b*h.
+"Se le piastrelle avessero un costo proporzionale alla loro area, come sulla Terra, la scelta del lato non farebbe alcuna differenza.
+"costo totale = lato della piastrella * numero piastrelle in orizzontale * numero piastrelle in verticale
+"             = l * no * nv
+"             = l * b/l * h/l
+"             = b*h/l
+"con b,h,l € N (naturali positivi).
+"Lim _l->+inf b*h/l = 0
+"Lim _l->0+ b*h/l = +inf
+" => l va massimizzato per minimizzare il costo
+"Per visualizzare ciò, basta graficare la formula c=b*h/l mettendo l sulle ascisse e il costo risultante sulle ordinate.
+"Quindi, come scegliere l?
+"Dovendo utilizzare tutte piastrelle quadrate e intere (non possiamo tagliarle), della stessa dimensione,
+"abbiamo che b e h devono essere entrambi divisibili per lo stesso l, che va massimizzato:
+"b/l, h/l € N  =>  l=MCD(b,h).
+"C.v.d.
